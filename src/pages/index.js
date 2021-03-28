7df8bc30280e5329704b83157228423bfd5b9911
Andrew Yang <andy.tubeee@gmail.com>
2021-03-28 16:09:35 -0400

    BufferSpinner.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import { navigate } from 'gatsby';
-import React, { memo, useState, useContext, useEffect } from "react";
+import React, { memo, useState, useContext, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { RiTwitterLine } from 'react-icons/ri';
 import { FcGoogle, VscPerson } from 'react-icons/all';
@@ -7,23 +7,22 @@ import background from '../images/register_bg_2.png';
 import Navbar from '../components/landing/Navbar';
 import FooterSmall from '../components/landing/FooterSmall';
 import UserContext from '../contexts/UserContext';
-import LoginPage from "../components/landing/LoginPage";
+import LoginPage from '../components/landing/LoginPage';
+import ProgressBar from '../components/shared/ProgressBar';
 
 function Home() {
-    const { user } = useContext(
-    UserContext,
-  );
+  const { user } = useContext(UserContext);
 
-  useEffect( () =>{
-    if (user){
-      navigate('/app/dashboard')
+  useEffect(() => {
+    if (user) {
+      navigate('/app/dashboard');
     }
-}
-  )
+  });
 
   return (
     <>
       <LoginPage />
+      <ProgressBar />
     </>
   );
 }
